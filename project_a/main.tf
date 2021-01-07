5603a8314df45e4597e3718250991371d861514a
limitpointinf0 <coneal0523@protonmail.com>
2021-01-07 17:52:10 +0700

    [ADD] init

---
+++ project_a/main.tf
@@ -0,0 +1,109 @@
+// Configure the Google Cloud provider
+provider "google" {
+    credentials = file(var.creds_path)
+    project     = var.project
+    region      = var.region
+}
+
+resource "random_id" "instance_id" {
+    byte_length = 8
+}
+
+//Create Subnets
+resource "google_compute_subnetwork" "vpc_subnet_a" {
+    name          = "a-subnet"
+    ip_cidr_range = "10.0.0.0/24"
+    region        = var.region
+    network       = google_compute_network.vpc_network.id
+}
+resource "google_compute_subnetwork" "vpc_subnet_b" {
+    name          = "b-subnet"
+    ip_cidr_range = "10.1.0.0/24"
+    region        = var.region_us
+    network       = google_compute_network.vpc_network.id
+}
+
+//Create Networks
+resource "google_compute_network" "vpc_network" {
+    name = "a-network"
+    auto_create_subnetworks = false
+    routing_mode = "REGIONAL"
+    mtu = 1500
+}
+
+// Add Firewall Rule
+resource "google_compute_firewall" "vpc_firewall" {
+    name    = "allow-ssh"
+    network = google_compute_network.vpc_network.name
+
+    allow {
+        protocol = "icmp"
+    }
+
+    allow {
+        protocol = "tcp"
+        ports    = ["22"]
+    }
+}
+
+// Add Peering
+resource "google_compute_network_peering" "vpc_peering" {
+  name         = "peer-skynet"
+  network      = google_compute_network.vpc_network.id
+  peer_network = var.project_b
+}
+
+// VM 1
+resource "google_compute_instance" "vm_a" {
+    name         = "vma-${random_id.instance_id.hex}"
+    machine_type = var.size
+    zone         = var.zone_ase
+
+    boot_disk {
+        initialize_params {
+            image = var.image
+        }
+    }
+
+    metadata = {
+        ssh-keys = "chris:${file("~/.ssh/id_rsa.pub")}"
+    }
+
+    network_interface {
+        network = google_compute_network.vpc_network.name
+        subnetwork = google_compute_subnetwork.vpc_subnet_a.name
+        access_config {}
+    }
+}
+
+// VM 2
+resource "google_compute_instance" "vm_b" {
+    name         = "vmb-${random_id.instance_id.hex}"
+    machine_type = var.size
+    zone         = var.zone_us
+
+    boot_disk {
+        initialize_params {
+            image = var.image
+        }
+    }
+
+    metadata = {
+        ssh-keys = "chris:${file("~/.ssh/id_rsa.pub")}"
+    }
+
+    network_interface {
+        network = google_compute_network.vpc_network.name
+        subnetwork = google_compute_subnetwork.vpc_subnet_b.name
+        access_config {}
+    }
+}
+
+// External IP outputs
+output "vma-ip" {
+    value = google_compute_instance.vm_a.network_interface.0.access_config.0.nat_ip
+}
+
+output "vmb-ip" {
+    value = google_compute_instance.vm_b.network_interface.0.access_config.0.nat_ip
+}
---
+++ project_a/variables.tf
@@ -0,0 +1,35 @@
+variable "creds_path" {
+    default = "../../credentials.json"
+} 
+
+variable "project" {
+    default = "dark-depth-298212"
+} 
+
+variable "region" {
+    default = "asia-southeast1"
+} 
+
+variable "region_us" {
+    default = "us-central1"
+} 
+
+variable "zone_ase" {
+    default = "asia-southeast1-a"
+}
+
+variable "zone_us" {
+    default = "us-central1-a"
+}
+
+variable "image" {
+    default = "ubuntu-os-cloud/ubuntu-1804-lts"
+} 
+
+variable "size" {
+    default = "f1-micro"
+}
+
+variable "project_b" {
+    default = "/projects/project-vpc-301008/global/networks/c-network"
+} 
